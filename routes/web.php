fafdb6203bfa0b5c850b72ab584546af7c6533e9
Sangeeth Sujith <sangeethps2255@gmail.com>
2023-11-30 11:14:56 +0530

    dashboard updated

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('login');});
 Route::get('signup', function () { return view('signup');});
+Route::get('dashboard', function () { return view('dashboard');});
